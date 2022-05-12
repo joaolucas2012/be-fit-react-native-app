c374fcf47bd84a750111c61c212a8c65af77d42d
unknown <ribeirojoaolucas68@gmail.com>
2022-05-11 23:41:02 -0300

    feat: update the app colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,6 +1,11 @@
 export default{
-    black: '#667080',
+    black: '#565656',
     white: '#FFFFFF',
     primary: '#30A960',
     secondary: '#EEF1F4',
+
+    heading: '#000000',
+    text: '#3F3D3D',
+
+    lightgrey: '#E5E7EA',
 };
