c28f0328e0ee20a700e716ed1707b4e7d6585c98
unknown <ribeirojoaolucas68@gmail.com>
2022-05-18 18:44:49 -0300

    feat: add the component of weight and height

---
+++ src/components/WeightAndHeight.tsx
@@ -0,0 +1,83 @@
+import react from "react";
+
+import {
+    View,
+    Text,
+    StyleSheet
+} from "react-native";
+
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+interface measurementsProps{
+    weight: string;
+    height: string;
+}
+
+export function WeightAndHeight(
+    {
+        weight,
+        height
+    }:measurementsProps
+){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.bodyMetricsText}>
+                Medidas corporais
+            </Text>
+            <View style={styles.content}>
+                <View style={styles.weightView}>
+                    <Text style={styles.header}>
+                        Peso
+                    </Text>
+                    <Text style={styles.header}>
+                        {`${weight}kg`}
+                    </Text>
+                </View>
+                <View style={styles.heightView}>
+                    <Text style={styles.header}>
+                        Altura
+                    </Text>
+                    <Text style={styles.header}>
+                        {`${height}m`}
+                    </Text>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '95%',
+    },
+    bodyMetricsText: {
+        fontFamily: fonts.title,
+        fontSize: 18,
+        color: colors.black,
+        marginVertical: 10,
+    },
+    content: {
+        backgroundColor: colors.white,
+        borderRadius: 10,
+        width: "100%",
+        paddingHorizontal: 15,
+    },
+    header: {
+        fontFamily: fonts.complement,
+        fontSize: 18,
+        color: colors.black,
+        lineHeight: 40,
+    },
+    weightView: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    heightView: {
+        borderTopColor: colors.lightgrey,
+        borderTopWidth: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+});
