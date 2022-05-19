c82a5ed4e4bf66f808f9c8b162d7d01c0d881662
unknown <ribeirojoaolucas68@gmail.com>
2022-05-19 20:36:14 -0300

    feat: add the page of daily training

---
+++ src/pages/DailyTraining.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import {
+    SafeAreaView,
+    View,
+    FlatList,
+    StyleSheet
+} from "react-native";
+
+import colors from "../styles/colors";
+
+import { Button } from "../components/Button";
+import { PersonalIconsFooter } from "../components/PersonalIconsFooter";
+import { VideoCard } from "../components/VideoCard";
+import { SectionHeader } from "../components/SectionHeader";
+
+export function DailyTraining(){
+    return(
+        <SafeAreaView style={styles.container}>
+            <View style={styles.content}>
+                <SectionHeader 
+                    title="Terça-feira"
+                />
+                <FlatList
+                    data={[1,2,3]}
+                    renderItem={( item ) => (
+                        <VideoCard title="Agachamento com pesos" />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={styles.listStyle}
+                    numColumns={1}
+                />
+            </View>
+            <View>
+                <View style={styles.buttonView}>
+                    <Button
+                        title="Adicionar treino"
+                    />
+                </View>
+                <PersonalIconsFooter />
+            </View>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: "100%",
+        backgroundColor: colors.lightgrey,
+        justifyContent: "space-between",
+    },
+    content: {
+        marginLeft: 30,
+    },
+    listStyle: {
+        marginRight: 20,
+        marginTop: 20,
+    },
+    buttonView: {
+        alignItems: "center",
+        marginBottom: 40,
+    },
+});
